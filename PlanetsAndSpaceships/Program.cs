b4b2b0723c38e66fbb62158ef79463b40b25f0d3
Kelly Stoops <kstoops815@gmail.com>
2018-01-25 18:56:50 -0600

    planet list manipulation done

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -19,11 +19,36 @@ namespace PlanetsAndSpaceships
 
             planetList.AddRange(lastTwoPlanets);
 
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+
+            Console.WriteLine("All planets list");
             foreach (var item in planetList)
             {
                 Console.WriteLine(item);
             }
 
+            //Use getRange to remove the rocky planets and create a rockyPlanets list
+
+            List<string> rockyPlanets = planetList.GetRange(0, 3);
+
+            Console.WriteLine("Rocky Planets list");
+            foreach (var rockyPlanet in rockyPlanets)
+            {
+                Console.WriteLine(rockyPlanet);
+
+            }
+            //Remove Pluto from planet list
+            Console.WriteLine("Pluto removed from list");
+
+            planetList.Remove(planetList.Last());
+
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+            
             Console.Read();
         }
     }
